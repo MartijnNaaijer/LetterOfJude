26fc8c5b3843dddba4ab4741f0849441ae281dd3
Martijn Naaijer <martijn.naayer@chello.nl>
2022-03-14 18:22:19 +0100

    save data in repo, solved unlinked verses and lacunae

---
+++ tf/otext.tf
@@ -0,0 +1,15 @@
+@config
+@author=Tommy Wasserman
+@contributors=Jan Krans, Dirk Roorda
+@converter=Martijn Naaijer
+@fmt:line-default={greek:XXX} 
+@fmt:text-orig-full={greek} 
+@name=TRANSCRIPTION OF THE MANUSCRIPTS CONTAINING THE NEW TESTAMENT LETTER OF JUDE
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@source=Data collected and edited by Tommy Wasserman
+@url=https://easy.dans.knaw.nl/ui/datasets/id/easy-dataset:52008
+@version=0.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-03-14T17:20:08Z
+
---
+++ tf/otype.tf
@@ -0,0 +1,18 @@
+@node
+@author=Tommy Wasserman
+@contributors=Jan Krans, Dirk Roorda
+@converter=Martijn Naaijer
+@name=TRANSCRIPTION OF THE MANUSCRIPTS CONTAINING THE NEW TESTAMENT LETTER OF JUDE
+@source=Data collected and edited by Tommy Wasserman
+@url=https://easy.dans.knaw.nl/ui/datasets/id/easy-dataset:52008
+@valueType=str
+@version=0.1
+@writtenBy=Text-Fabric
+@dateWritten=2022-03-14T17:20:05Z
+
+1-1462661	sign
+1462662-1463225	book
+1463226-1463789	chapter
+1463790-1464353	document
+1464354-1479577	verse
+1479578-1730158	word
